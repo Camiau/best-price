c8c62fdead9440fdfe7969473ee61d86f6c782a5
Gaston Harari <gastonh_lu@lagash.com>
2017-12-04 10:09:05 -0300

    add reference from api to common

--- MejorPrecio.Api/ProductsApi.cs
+++ MejorPrecio.Api/ProductsApi.cs
@@ -1,8 +1,14 @@
 ﻿using System;
-
+using MejorPrecio.Common;
 namespace MejorPrecio.Api
 {
     public class ProductsApi
     {
+        public Product SearchByCodeBar(long codeBar)
+        {
+            //codeBar is a valid codeBar chechekd by a previous function
+            var ret = new Product();
+            return ret;
+        }
     }
 }
