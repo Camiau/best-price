4bbfff5ba0aa38aa17e01cd1b0146a7f6ffd24ce
Gaston Harari <gastonh_lu@lagash.com>
2017-12-05 12:18:36 -0300

    adding all the information from: Rol,Price,User

--- MejorPrecio.Common/Price.cs
+++ MejorPrecio.Common/Price.cs
@@ -4,6 +4,12 @@ namespace MejorPrecio.Common
 {
     public class Price
     {
-        
+        int id;
+        string codeBar;//foreign key from a specific product
+        double PriceEffective;
+        DateTimeOffset date;
+        long lattitude;
+        long longittude;
+        ApplicationUser user;
     }
 }
--- MejorPrecio.Common/Product.cs
+++ MejorPrecio.Common/Product.cs
@@ -4,5 +4,7 @@ namespace MejorPrecio.Common
 {
     public class Product
     {
+        string codeBar;
+        string[] description;
     }
 }
