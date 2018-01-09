6ff3d437c4a982966a25a378aff649383349efa4
Gaston Harari <gastonh_lu@lagash.com>
2018-01-09 10:44:52 -0300

    added PermissionRepository.cs

---
+++ MejorPrecio.Persistence/PermissionRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MejorPrecio.Common;
+using System.Data.SqlClient;
+using System.Data;
+
+public class PermissionRepository
+{
+    private static string conectionStringLocalDB = Environment.GetEnvironmentVariable("conectionStringLocalDB");
+    
+}
