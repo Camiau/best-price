38e6ffcba10d4d6cb5016b6eaa9974097d731b4f
Fernando Guida <fernandog_lu@lagash.com>
2018-01-15 11:33:23 -0300

    Cambios funcionando

--- MejorPrecio.MvcView/Controllers/ProductsController.cs
+++ MejorPrecio.MvcView/Controllers/ProductsController.cs
@@ -5,19 +5,18 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using MejorPrecio.MvcView.Models;
 using Microsoft.AspNetCore.Http;
-using System.Net.Http;
-using System.IO;
-using System.Net.Http.Headers;
-using Newtonsoft.Json;
-using System.Text;
+using MejorPrecio.Api;
+using MejorPrecio.Common;
 
 namespace MejorPrecio.MvcView.Controllers
 {
     public class ProductsController : Controller
     {
+        private ProductsApi logic = new ProductsApi();
+
 
         [HttpGet]
-        public IActionResult Search()
+        public IActionResult Index()
         {
 
             return View();
@@ -26,54 +25,17 @@ namespace MejorPrecio.MvcView.Controllers
         [HttpPost]
         public IActionResult Search(IFormFile image)
         {
-            HttpClientHandler clientHandler = new HttpClientHandler();
 
-            clientHandler.AllowAutoRedirect = false;
-
-            using (HttpClient client = new HttpClient(clientHandler))
-            {
-
-                client.BaseAddress = new Uri("http://localhost:5003/");
-                MultipartFormDataContent form = new MultipartFormDataContent();
-
-                try
-                {
-                    using (var ms = new MemoryStream())
-                    {
-                        image.CopyTo(ms);
-                        var fileBytes = ms.ToArray();
-                        string s = Convert.ToBase64String(fileBytes);
-                        HttpContent content = new StringContent(s);
-                        form.Add(content, "file");
-                    }
-                    
-                    var response = client.PostAsync("api/Products", form).Result;
-
-                    return View();
-                }
-                catch (Exception)
-                {
-                    return StatusCode(501); // 500 is generic server error
-                }
-            }
-
-            /*using (MemoryStream m = new MemoryStream())
-            {
-                file.CopyTo(m);
-                byte[] imageBytes = m.ToArray();
-
-                // Convert byte[] to Base64 String
-                string base64String = Convert.ToBase64String(imageBytes);
-            }
-
-            return StatusCode(200);*/
+            var code = logic.GetBarcode(image.OpenReadStream());
+            
+            return RedirectToAction("Search", new { barCode = code });
         }
 
-
+        [HttpGet]
         public IActionResult NewProduct()
         {
-
-            return View();
+            var barCode= TempData["BarCode"];     
+            return View(barCode);
         }
 
         [HttpPost]
@@ -84,6 +46,25 @@ namespace MejorPrecio.MvcView.Controllers
                 return View();
             }
             return View(product.description);
+        }
+
+        [HttpGet]
+        public IActionResult Search(string barCode)
+        {
+            if(barCode == null || barCode.Length == 0)
+            {
+                ModelState.AddModelError(String.Empty, "Debes ingresar un parámetro de búsqueda.");
+                return View();
+
+            }
+            var product = logic.SearchByBarCode(barCode);
+            if(product == null)
+            {
+                TempData["BarCode"] = barCode;
+                return RedirectToAction("NewProduct");
+            }
+
+            return View(product);
         }
     }
 }
