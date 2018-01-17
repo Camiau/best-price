16b9d6de303b266ce6838ac0adf834ea3226d7ea
Camila Forestiero <camilaf_lu@lagash.com>
2018-01-17 10:45:04 -0300

    pines en el mapa para product harcodeado

--- MejorPrecio.MvcView/Controllers/PriceController.cs
+++ MejorPrecio.MvcView/Controllers/PriceController.cs
@@ -1,20 +1,45 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using MejorPrecio.MvcView.Models;
+using MejorPrecio.Api;
+using System.Net.Http;
+using MejorPrecio.Common;
+
+
 
 namespace MejorPrecio.MvcView.Controllers
 {
     public class PriceController : Controller
     {
+         
         public IActionResult Index()
         {
             ViewData["Message"] = "Your application description page.";
 
             return View("NewPrice");
         }
+
+[HttpGet]
+        public IActionResult Map()
+        {
+            /* var api = new PricesApi();
+            var prod = new Product();
+            prod.IdProduct = idProd;
+
+            ViewBag.PriceList = api.FindBestPrice(prod); */
+            return View("ViewMap");
+        }
+
+        public List<Price> ViewPrices(Guid idProd)
+        {
+            var api = new PricesApi();
+            var prod = new Product();
+            prod.IdProduct = idProd;
+
+            return api.FindBestPrice(prod);
+        }
+
+
     }
 }
