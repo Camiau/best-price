0e9f8345cfad64b8c9e71d9270045cb7732d53b8
Fernando Guida <fernandog_lu@lagash.com>
2018-01-24 09:59:41 -0300

    Agregadas funciones por defecto en el javascript de New Product

--- MejorPrecio.MvcView/wwwroot/js/newProduct.js
+++ MejorPrecio.MvcView/wwwroot/js/newProduct.js
@@ -1,7 +1,10 @@
 function initMap() {
+
+  //Posicion por defecto por si el usuario no tiene geolocalizacion en el navegador
+  var defaultCenter = { lat: -34.608433, lng: -58.37251 };
   //inicializamos el mapa
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.608433, lng: -58.37251 },
+    center: defaultCenter,
     zoom: 17
   });
 
@@ -56,21 +59,60 @@ function initMap() {
       });
 
     }, function () {
-      handleLocationError(true, infoWindow, map.getCenter());
+      //Si falla algo en obtener la geolocalizacion del usuario cae en esta funcion
+      handleLocationError();
     });
 
   }
 
   else {
     // El navegador no soporta la geolocalización
-    handleLocationError(false, infoWindow, map.getCenter());
+    handleLocationError();
   }
 
+  function handleLocationError() {
+    //Posicionamos el centro del mapa en la posicion por defecto
+    map.setCenter(defaultCenter);
 
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-      'Error: El servicio de geolocalizacion fallo.' :
-      'Error: Tu navegador no soporta geolocalizacion.');
+    //Posicionamos el marcador en el mapa en la ubicacion por defect
+    var marker = new google.maps.Marker({
+      position: defaultCenter,
+      map: map,
+      draggable: true,
+      title: 'Elegir ubicación del producto'
+    });
+
+
+    //Variables para obtener la direccion del lugar donde apunta el usuario con el marcador
+    let address = new google.maps.LatLng(defaultCenter.lat, defaultCenter.lng);
+    //Inicializamos la geolocalizacion reversa
+    let geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({
+      location: address
+    }, function (geocoderResults) {
+      document.getElementById("addr").value = geocoderResults[0].address_components[1].short_name + ' ' + geocoderResults[0].address_components[0].long_name;
+    });
+
+    //Rellenamos los inputs de direccion con la latitud y longitud
+    document.getElementById("lat").value = defaultCenter.lat;
+
+    document.getElementById("lng").value = defaultCenter.lng;
+
+    //Rellenamos los inputs esta vez cuando el usuario mueve el marcador
+    google.maps.event.addListener(marker, 'dragend', function (event) {
+
+      geocoder.geocode({
+        location: new google.maps.LatLng(event.latLng.lat(), event.latLng.lng())
+      }, function (geocoderResults) {
+        document.getElementById("addr").value = geocoderResults[0].address_components[1].short_name + ' ' + geocoderResults[0].address_components[0].long_name;
+      });
+
+      document.getElementById("lat").value = event.latLng.lat();
+
+      document.getElementById("lng").value = event.latLng.lng();
+
+    });
+
   }
 }
